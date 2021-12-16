1c82982343217f0f498c66d04575750b12327eb5
Alex Traill <>
2021-12-16 13:31:20 +0100

    Updated header, homepage hero and removed footer

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function myFunction() {
     var x = document.getElementById("myLinks");
+    var y = document.getElementById("socialLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
+        y.style.display = "none";
     } else {
         x.style.display = "block";
+        y.style.display = "flex";
     }
 }
 
